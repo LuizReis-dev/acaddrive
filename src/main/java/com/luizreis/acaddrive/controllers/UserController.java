3d7237c3012bb71658233d7e9a1fb2700e8fb852
Luiz Reis <reisluiz05@gmail.com>
2023-06-17 15:25:55 -0300

    refact user insert controller

--- src/main/java/com/luizreis/acaddrive/controllers/UserController.java
+++ src/main/java/com/luizreis/acaddrive/controllers/UserController.java
@@ -5,10 +5,14 @@ import com.luizreis.acaddrive.dto.user.UserResponseDTO;
 import com.luizreis.acaddrive.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -22,7 +26,14 @@ public class UserController {
     }
 
     @PostMapping
-    public UserResponseDTO insert(@Valid @RequestBody UserRequestDTO dto){
-        return service.insert(dto);
+    public ResponseEntity<UserResponseDTO> insert(@Valid @RequestBody UserRequestDTO dto){
+        UserResponseDTO responseDTO = service.insert(dto);
+        URI uri = ServletUriComponentsBuilder
+                .fromCurrentRequest()
+                .path("/{id}")
+                .buildAndExpand(responseDTO.getId())
+                .toUri();
+
+        return ResponseEntity.created(uri).body(responseDTO);
     }
 }
