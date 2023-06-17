8264988cd5f624ed4f342931f6fc628599e9ee7e
Luiz Reis <reisluiz05@gmail.com>
2023-06-17 10:06:14 -0300

    Project created

---
+++ src/main/java/com/luizreis/acaddrive/AcaddriveApplication.java
@@ -0,0 +1,13 @@
+package com.luizreis.acaddrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcaddriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcaddriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luizreis/acaddrive/AcaddriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luizreis.acaddrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AcaddriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
