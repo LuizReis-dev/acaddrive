3739490140a222c0658d7e42d8a8220cb49a3568
Luiz Reis <reisluiz05@gmail.com>
2023-06-17 15:33:48 -0300

    get all users paged

--- src/main/java/com/luizreis/acaddrive/controllers/UserController.java
+++ src/main/java/com/luizreis/acaddrive/controllers/UserController.java
@@ -5,11 +5,10 @@ import com.luizreis.acaddrive.dto.user.UserResponseDTO;
 import com.luizreis.acaddrive.services.UserService;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
@@ -23,6 +22,11 @@ public class UserController {
     @Autowired
     public UserController(UserService service) {
         this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<Page<UserResponseDTO>> findAll(Pageable pageable){
+        return ResponseEntity.ok(service.findAll(pageable));
     }
 
     @PostMapping
--- src/main/java/com/luizreis/acaddrive/services/UserService.java
+++ src/main/java/com/luizreis/acaddrive/services/UserService.java
@@ -7,8 +7,13 @@ import com.luizreis.acaddrive.repositories.UserRepository;
 import com.luizreis.acaddrive.services.exceptions.DbViolationException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class UserService {
@@ -22,6 +27,7 @@ public class UserService {
         this.passwordEncoder = passwordEncoder;
     }
 
+    @Transactional
     public UserResponseDTO insert(UserRequestDTO dto){
         try {
             User user = new User();
@@ -32,5 +38,11 @@ public class UserService {
         }catch (DataIntegrityViolationException e){
             throw new DbViolationException("This email is already in use");
         }
+    }
+
+    @Transactional(readOnly = true)
+    public Page<UserResponseDTO> findAll(Pageable pageable) {
+        Page<User> users = repository.findAll(pageable);
+        return users.map(user -> new UserResponseDTO(user));
     }
 }
