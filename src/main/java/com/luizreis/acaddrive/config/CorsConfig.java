70fb7d2c10ff92d4d97cd9d05d656998887b51bb
Luiz Reis <reisluiz05@gmail.com>
2023-07-02 20:12:19 -0300

    cors config

--- src/main/java/com/luizreis/acaddrive/config/CorsConfig.java
+++ src/main/java/com/luizreis/acaddrive/config/CorsConfig.java
@@ -8,7 +8,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry){
         registry.addMapping("/**")
-                .allowedOrigins("*")
+                .allowedOrigins("http://127.0.0.1:5500")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "HEAD", "TRACE", "CONNECT");
     }
 }
